9808176cf1766b8bbb9c8aea512b18caa83bca64
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2024-09-26 18:44:40 +1200

    Added public API change checker
    https://andrewlock.net/preventing-breaking-changes-in-public-apis-with-publicapigenerator/

---
+++ src/ZeroRedact.UnitTest/PublicApi/PublicApiTest.cs
@@ -0,0 +1,16 @@
+﻿using PublicApiGenerator;
+
+namespace ZeroRedact.UnitTest.PublicApi
+{
+    [TestClass]
+    public class PublicApiTest : VerifyBase
+    {
+        [TestMethod]
+        public Task CheckForPublicApiChanges()
+        {
+            var publicApi = typeof(Redactor).Assembly.GeneratePublicApi();
+
+            return Verifier.Verify(publicApi);
+        }
+    }
+}
