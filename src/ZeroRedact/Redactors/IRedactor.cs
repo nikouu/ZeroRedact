63739320fa5b651160bb54d3c8f58deac51b4357
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2024-09-30 17:08:05 +1300

    Introduced breaking change moving namespaces
    And changing parameter names in a previous commit.

--- src/ZeroRedact/Redactors/IRedactor.cs
+++ src/ZeroRedact/Redactors/IRedactor.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net.Mail;
 
-namespace ZeroRedact.Redactors
+namespace ZeroRedact
 {
     /// <summary>
     /// Implements redaction capabilities.
--- src/ZeroRedact/Redactors/Redactor.Common.cs
+++ src/ZeroRedact/Redactors/Redactor.Common.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using ZeroRedact.Redactors;
 
 namespace ZeroRedact
 {
