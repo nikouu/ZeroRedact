87a5f711dd31480c17d5c26abb446c7f55589d2a
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2025-02-15 13:05:03 +1300

    Removed unused parameter in MACAddressValidator

--- src/ZeroRedact/Validators/MACAddressValidator.cs
+++ src/ZeroRedact/Validators/MACAddressValidator.cs
@@ -7,15 +7,15 @@
             var octetCountColon = macAddress.Count(':');
             var octetCountDash = macAddress.Count('-');
 
-            return IsEui48(macAddress, octetCountColon, octetCountDash) || IsEui64(macAddress, octetCountColon, octetCountDash);
+            return IsEui48(octetCountColon, octetCountDash) || IsEui64(octetCountColon, octetCountDash);
         }
 
-        private static bool IsEui48(ReadOnlySpan<char> macAddress, int octetCountColon, int octetCountDash)
+        private static bool IsEui48(int octetCountColon, int octetCountDash)
         {
             return octetCountColon == 5 || octetCountDash == 5;
         }
 
-        private static bool IsEui64(ReadOnlySpan<char> macAddress, int octetCountColon, int octetCountDash)
+        private static bool IsEui64(int octetCountColon, int octetCountDash)
         {
             return octetCountColon == 7 || octetCountDash == 7;
         }
